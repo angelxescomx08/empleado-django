cb5aed881faac5244a37486b5b61c2aa91f1adc1
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-02-12 21:27:09 -0600

    Add Departamento model and register in admin

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Departamento(models.Model):
+    #nombre del campo en el admin
+    name = models.CharField("Nombre", max_length=50, blank=True, null=True)
+    short_name = models.CharField("Nombre corto", max_length=20, unique=True)
+    anulate = models.BooleanField("Anulado", default=False)
+
+    def __str__(self) -> str:
+        return str(self.id) + "-" + self.name + "-" + self.short_name
