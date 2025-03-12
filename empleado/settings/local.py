e8a333a3c1cef8715241ad4c962c16e1ecf9a5a1
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-03-11 22:29:54 -0600

    Add Docker support and configure PostgreSQL database settings

--- empleado/settings/local.py
+++ empleado/settings/local.py
@@ -9,9 +9,13 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "empleado_db",
+        "USER": "empleado_user",
+        "PASSWORD": "123456789",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -19,4 +23,4 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
